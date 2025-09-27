f70d822b6cd7e5c3683bbdca837c12bc41392664
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-09-27 18:28:20 +0200

    fix: Update server address in consumer script

--- kafkaconnector_tools/consumer.sh
+++ kafkaconnector_tools/consumer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -cp "target/kafkaconnector_tools-1.0-SNAPSHOT.jar:target/dependency/*:dependency/log4j2.xml" \
-    com.lightstreamer.LightstreamerConsumer --server-address=http://ec2-54-217-184-231.eu-west-1.compute.amazonaws.com:8080 \
+    com.lightstreamer.LightstreamerConsumer --server-address=http://ec2-34-244-144-102.eu-west-1.compute.amazonaws.com:8080 \
     --calculate-latency-stats
