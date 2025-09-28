b7387e59273365a3d8718f1e88c690009f210b94
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-09-28 19:03:55 +0200

    feat: Add SimpleProducer class for enhanced message generation

---
+++ kafkaconnector_tools/src/main/java/com/lightstreamer/SimpleProducer.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2024 Lightstreamer Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.lightstreamer;
+
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.Serdes.StringSerde;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProducer extends BaseProducer {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    String[] strings = { "Apple", "Banana", "Orange", "Grape", "Pineapple",
+            "Strawberry", "Watermelon", "Mango", "Kiwi", "Lemon",
+            "Peach", "Cherry", "Blueberry", "Raspberry", "Blackberry",
+            "Coconut", "Pomegranate", "Cantaloupe", "Apricot", "Fig",
+            "Plum", "Pear", "Avocado", "Lychee", "Guava",
+            "Dragonfruit", "Passionfruit", "Papaya", "Melon", "Lime",
+            "Nectarine", "Persimmon", "Starfruit", "Tangerine", "Durian",
+            "Kumquat", "Cranberry", "Rambutan", "Mangosteen", "Jackfruit" };
+
+    private String[] largeStrings = new String[strings.length];
+
+    private static final Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+
+    private static final Random random = new SecureRandom();
+
+    private static String generateRandomString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+
+    private int generateRndInt() {
+        return random.nextInt();
+    }
+
+    private static String generateMillisTS() {
+        return Instant.now().toString();
+    }
+
+    private static String buildRepeatedString(String base, int totalLength) {
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < totalLength) {
+            sb.append(base);
+        }
+        return sb.length() > totalLength
+                ? sb.substring(0, totalLength)
+                : sb.toString();
+    }
+
+    private boolean addPrefix;
+    private boolean useLargeStrings;
+    private AtomicLong globalMessageCount;
+
+    public SimpleProducer(AtomicLong globalMessageCount, String kafka_bootstrap_string, String pid,
+            String topicname,
+            int pause, int msgsize,
+            boolean addPrefix, boolean useLargeStrings) {
+        super(kafka_bootstrap_string, pid, topicname, pause, msgsize);
+        this.globalMessageCount = globalMessageCount;
+        this.addPrefix = addPrefix;
+        this.useLargeStrings = useLargeStrings;
+        logger.info("Protobuf producer: " + pid + ", prefix: " + addPrefix + ", ok.");
+
+        for (int i = 0; i < strings.length; i++) {
+            largeStrings[i] = buildRepeatedString(strings[i], 500);
+        }
+    }
+
+    @Override
+    public void run() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", kafkabootstrapstring);
+        props.put("linger.ms", 50);
+        props.put("acks", "0");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                org.apache.kafka.common.serialization.StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                org.apache.kafka.common.serialization.StringSerializer.class);
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        publish(producer);
+    }
+
+    private void publish(Producer<String, String> producer) {
+        Instant starInstant = Instant.now();
+        while (true) {
+            String prefix = addPrefix ? "PREFIX-" : "";
+            String[] keyArray = useLargeStrings ? largeStrings : strings;
+            int index = random.nextInt(keyArray.length);
+            String sndV = keyArray[index];
+
+            String message = generateMillisTS();
+            logger.debug("ProducerId - {}, New message for :{}", producerid, message.toString());
+            try {
+                producer.send(new ProducerRecord<>(ktopicname, sndV, message),
+                        (metadata, exception) -> {
+                            if (exception != null) {
+                                logger.error("Error while producing message to topic : " + metadata.topic(),
+                                        exception);
+                                return;
+                            }
+
+                            Instant now = Instant.now();
+                            Duration elapsed = Duration.between(starInstant, now);
+                            logger.info("ProducerId - {} - Sent {} in {} seconds", producerid,
+                                    globalMessageCount.incrementAndGet(), elapsed.toSeconds());
+                        });
+            } catch (Exception e) {
+                logger.error("Error during sending message : " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public static class ProtoTestObjSerializer implements Serializer<com.lightstreamer.proto.TestObj> {
+
+        @Override
+        public byte[] serialize(String topic, com.lightstreamer.proto.TestObj data) {
+            if (data == null) {
+                return null;
+            }
+            return data.toByteArray();
+        }
+    }
+}
