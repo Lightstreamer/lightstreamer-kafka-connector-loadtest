66f7e1240ce8876b6daadf96b972838e8992f11f
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-09-29 10:56:07 +0200

    fix: Update publish script to correct producer count and cluster configuration

--- kafkaconnector_tools/publish.sh
+++ kafkaconnector_tools/publish.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #mvn clean package dependency:copy-dependencies
 TOPIC=LTest
-PRODUCER=2
-PAUSE=50
-MESSAGE_SIZE=1024
-java -cp "target/kafkaconnector_tools-1.0-SNAPSHOT.jar:target/dependency/*:dependency/log4j2.xml" com.lightstreamer.MessageGenerator ec2-54-74-235-5.eu-west-1.compute.amazonaws.com:9092 $TOPIC $PRODUCER $PAUSE $MESSAGE_SIZE protobuf
+PRODUCERS=1
+KAFKA_CLUSTER=localhost:9092
+java -cp "target/kafkaconnector_tools-1.0-SNAPSHOT.jar:target/dependency/*:dependency/log4j2.xml" com.lightstreamer.MessageGenerator \
+    ${KAFKA_CLUSTER} \
+    $TOPIC \
+    $PRODUCERS 500 1024 \
+    simple
