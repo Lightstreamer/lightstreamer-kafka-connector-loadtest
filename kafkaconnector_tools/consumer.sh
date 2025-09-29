2290bd8f70d0dbd7b895a555c65969e2376fc1e8
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-09-29 10:49:05 +0200

    feat: Refactor consumer scripts for improved Kafka connection handling

--- kafkaconnector_tools/consumer.sh
+++ kafkaconnector_tools/consumer.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-java -cp "target/kafkaconnector_tools-1.0-SNAPSHOT.jar:target/dependency/*:dependency/log4j2.xml" \
-    com.lightstreamer.LightstreamerConsumer --server-address=http://ec2-34-244-144-102.eu-west-1.compute.amazonaws.com:8080 \
-    --calculate-latency-stats
+set -ex
+#mvn clean package dependency:copy-dependencies
+KAFKA_CLUSTER=ec2-3-250-39-145.eu-west-1.compute.amazonaws.com:9092
+TOPIC=LTest
+CONSUMERS=1
+CONSUMER_GROUP_ID=simple
+java -cp "target/kafkaconnector_tools-1.0-SNAPSHOT.jar:target/dependency/*:dependency/log4j2.xml" com.lightstreamer.Main \
+    $KAFKA_CLUSTER \
+    $TOPIC \
+    $CONSUMERS \
+    $CONSUMER_GROUP_ID \
+    true
