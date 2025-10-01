48dee15b100a9e1f5bfd03794e599e1ef78ee65e
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-01 12:09:59 +0200

    fix: Update KAFKA_CLUSTER address and JAR version in publish script

--- kafkaconnector_tools/publish.sh
+++ kafkaconnector_tools/publish.sh
@@ -3,8 +3,8 @@
 TOPIC=LTest
 PRODUCERS=1
 # KAFKA_CLUSTER=localhost:9092
-KAFKA_CLUSTER=ec2-3-250-39-145.eu-west-1.compute.amazonaws.com:9092
-java -cp "target/kafkaconnector_tools-1.0-SNAPSHOT.jar:target/dependency/*:dependency/log4j2.xml" com.lightstreamer.MessageGenerator \
+KAFKA_CLUSTER=ec2-18-201-235-33.eu-west-1.compute.amazonaws.com:9092
+java -cp "target/original-kafkaconnector_tools-1.0.1-SNAPSHOT.jar:target/dependency/*" com.lightstreamer.MessageGenerator \
     ${KAFKA_CLUSTER} \
     $TOPIC \
     $PRODUCERS 500 1024 \
