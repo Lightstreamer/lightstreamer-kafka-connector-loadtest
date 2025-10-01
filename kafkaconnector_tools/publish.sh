eb9a56275345753f1a414f835579171d7994b0bc
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-01 18:55:34 +0200

    feat: Update publish script to use JAR for message generation with simplified parameters

--- kafkaconnector_tools/publish.sh
+++ kafkaconnector_tools/publish.sh
@@ -4,8 +4,4 @@ TOPIC=LTest
 PRODUCERS=1
 # KAFKA_CLUSTER=localhost:9092
 KAFKA_CLUSTER=ec2-18-201-235-33.eu-west-1.compute.amazonaws.com:9092
-java -cp "target/original-kafkaconnector_tools-1.0.1-SNAPSHOT.jar:target/dependency/*" com.lightstreamer.MessageGenerator \
-    ${KAFKA_CLUSTER} \
-    $TOPIC \
-    $PRODUCERS 500 1024 \
-    protobuf
+java -jar target/kafkaconnector-tools-message-generator.jar ${KAFKA_CLUSTER} $TOPIC $PRODUCERS 500 1024 simple
