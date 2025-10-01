8ec65d28061d58a0ff795d11fe5c2e7011a50317
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-01 12:09:47 +0200

    fix: Update JAR version in ls-consumer script to 1.0.1-SNAPSHOT

--- kafkaconnector_tools/ls-consumer.sh
+++ kafkaconnector_tools/ls-consumer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar target/kafkaconnector_tools-1.0-SNAPSHOT.jar http://ec2-54-217-187-209.eu-west-1.compute.amazonaws.com:8080
+java -jar target/kafkaconnector_tools-1.0.1-SNAPSHOT.jar http://ec2-54-217-187-209.eu-west-1.compute.amazonaws.com:8080
