ea3f71e9eeb96e7175fba08d1d0219e364b8fd00
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-01 13:09:40 +0200

    refactor: Rename thread for simple producer and simplify SimpleProducer instantiation

--- kafkaconnector_tools/src/main/java/com/lightstreamer/MessageGenerator.java
+++ kafkaconnector_tools/src/main/java/com/lightstreamer/MessageGenerator.java
@@ -111,22 +111,18 @@ public class MessageGenerator {
                 logger.info("Protobuf Producer pid-{} started.", k);
             }
         } else if (keyornot.equals("simple")) {
-            AtomicLong globalMessageCount = new AtomicLong(0);
             AtomicInteger threadId = new AtomicInteger(0);
             ExecutorService pool = Executors.newFixedThreadPool(num_producers, r -> {
                 Thread t = new Thread(r);
-                t.setName("Protobuf Publisher thread - " + threadId.incrementAndGet());
+                t.setName("Protobuf Simple thread - " + threadId.incrementAndGet());
                 return t;
             });
 
             for (int k = 0; k < num_producers; k++) {
-                producers[k] = new SimpleProducer(globalMessageCount, kconnstring, "pid-" + k, topicname, pause_milis,
-                        msg_size,
-                        additionalParam, lastParamForJson);
+                producers[k] = new SimpleProducer(kconnstring, "pid-" + k, topicname);
                 pool.submit(producers[k]);
-                // producers[k].start();
 
-                logger.info("Protobuf Producer pid-{} started.", k);
+                logger.info("Simple Producer pid-{} started.", k);
             }
 
         } else if (keyornot.equals("json2")) {
