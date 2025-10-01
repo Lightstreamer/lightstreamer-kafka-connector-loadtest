2b77a5cf8c1e76272965a063bfb057929a38473b
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-01 13:12:49 +0200

    fix: Remove extraneous argument from ls-consumer script execution

--- kafkaconnector_tools/ls-consumer.sh
+++ kafkaconnector_tools/ls-consumer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar target/kafkaconnector_tools-1.0.1-SNAPSHOT.jar http://ec2-54-217-187-209.eu-west-1.compute.amazonaws.com:8080 1
+java -jar target/kafkaconnector_tools-1.0.1-SNAPSHOT.jar http://ec2-54-217-187-209.eu-west-1.compute.amazonaws.com:8080
