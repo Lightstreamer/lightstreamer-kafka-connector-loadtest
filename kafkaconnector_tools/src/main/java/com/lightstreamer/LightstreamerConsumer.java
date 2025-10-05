6fbedb6dd8a73b8323a9ccaa45f5b0f26010c9f7
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-05 20:01:24 +0200

    fix: Adjust latency recording in LightstreamerConsumer to use milliseconds

--- kafkaconnector_tools/src/main/java/com/lightstreamer/LightstreamerConsumer.java
+++ kafkaconnector_tools/src/main/java/com/lightstreamer/LightstreamerConsumer.java
@@ -110,7 +110,7 @@ public class LightstreamerConsumer {
                 // long latency = System.nanoTime() - Long.parseLong(update.getValue("tradetime"));
                 long receivedTimestamp = Long.parseLong(update.getValue("route-latency"));
                 long latency = currentTimestamp - receivedTimestamp;
-                histogram.recordValue(latency);
+                histogram.recordValue(receivedTimestamp/1_000_000);
 
                 intervalMessageCounter++;
                 if (intervalMessageCounter == REPORT_INTERVAL_MESSAGE_COUNT) {
