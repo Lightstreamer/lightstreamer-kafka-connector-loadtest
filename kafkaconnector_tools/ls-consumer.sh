a9d706f8c85277e62096c2d1cfda1ea3dd9316fc
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-06 17:46:11 +0200

    feat: Update ls-consumer.sh to use named arguments for improved clarity and maintainability

--- kafkaconnector_tools/ls-consumer.sh
+++ kafkaconnector_tools/ls-consumer.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-java -jar target/ls-consumer.jar "http://${KAFKA_LS_PUBLIC_IP}:8080" $1 $2
+java -jar target/ls-consumer.jar \
+    --server "http://${KAFKA_LS_PUBLIC_IP}:8080" \
+    --clients 3 \
+    --keys 1000
