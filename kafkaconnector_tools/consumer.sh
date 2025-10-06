1255d8ee4d9a0120ea01f32dd55e61be0e4234fc
Gianluca <gianluca.finocchiaro@lightstreamer.com>
2025-10-06 12:41:10 +0200

    feat: Update consumer script to use Protobuf consumer group and optimized jar

--- kafkaconnector_tools/consumer.sh
+++ kafkaconnector_tools/consumer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -ex
-#mvn clean package dependency:copy-dependencies
-KAFKA_CLUSTER=ec2-3-250-39-145.eu-west-1.compute.amazonaws.com:9092
+
+# Kafka Consumer using optimized consumer.jar
+# Update these variables as needed for your environment
 TOPIC=LTest
 CONSUMERS=1
-CONSUMER_GROUP_ID=simple1
-java -cp "target/kafkaconnector_tools-1.0-SNAPSHOT.jar:target/dependency/*:dependency/log4j2.xml" com.lightstreamer.Main \
+CONSUMER_GROUP_ID=protobuf
+
+# Use the optimized consumer.jar instead of old classpath setup
+java -jar target/consumer.jar \
     $KAFKA_CLUSTER \
     $TOPIC \
     $CONSUMERS \
